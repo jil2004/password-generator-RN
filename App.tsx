0af9e320f4e3569047987763a95fc89fbc15a0a9
Jil <upadhyay.jil1802@gmail.com>
2024-03-12 15:49:48 +0530

    implement logic

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import * as Yup from 'yup' // Form Validation Package
 
 // Form validation using yup schema
 const PasswordSchema = Yup.object().shape({
   passwordLength: Yup.number()
-  .min(4, 'Should be at least 4 characters')
-  .max(16, 'Should not be more than 16 characters')
-  .required('Length is required')
+    .min(4, 'Should be at least 4 characters')
+    .max(16, 'Should not be more than 16 characters')
+    .required('Length is required')
 })
 export default function App() {
   const [password, setPassword] = useState('')
@@ -15,26 +15,58 @@ export default function App() {
 
   const [lowerCase, setLowerCase] = useState(true)
   const [upperCase, setUpperCase] = useState(false)
-  const [numbers, useNumbers] = useState(false)
-  const [symbols, useSymbols] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
 
   const generatePasswordString = (passwordLength: number) => {
-    //
+    let characterList = '';
+
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'ancdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+';
+
+    if (upperCase) {
+      characterList += upperCaseChars
+    }
+    if (lowerCase) {
+      characterList += lowerCaseChars
+    }
+    if (numbers) {
+      characterList += digitChars
+    }
+    if (symbols) {
+      characterList += specialChars
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength)
+    setPassword(passwordResult)
+    setIsPassGenerated(true)
   }
 
   const createPassword = (characters: string, passwordLength: number) => {
-    //
+    let result = ''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length)
+      result += characters.charAt(characterIndex)
+    }
+    return result
+
+    const resetPasswordState = () => {
+      setPassword('')
+      setIsPassGenerated(false)
+      setLowerCase(true)
+      setUpperCase(false)
+      setNumbers(false)
+      setSymbols(false)
+      
+    }
+
+    return (
+      <View>
+        <Text>App</Text>
+      </View>
+    )
   }
 
-  const resetPasswordState = () => {
-    // 
-  }
-
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({})
+  const styles = StyleSheet.create({})
