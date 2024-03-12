ea0ba6c7bcca0d871e237c73a003d2d48a6b168f
Jil <upadhyay.jil1802@gmail.com>
2024-03-12 14:58:26 +0530

    Add useState

--- App.tsx
+++ App.tsx
@@ -10,6 +10,26 @@ const PasswordSchema = Yup.object().shape({
   .required('Length is required')
 })
 export default function App() {
+  const [password, setPassword] = useState('')
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, useNumbers] = useState(false)
+  const [symbols, useSymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength: number) => {
+    //
+  }
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    //
+  }
+
+  const resetPasswordState = () => {
+    // 
+  }
+
   return (
     <View>
       <Text>App</Text>
